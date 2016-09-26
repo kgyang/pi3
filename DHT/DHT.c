1b32ff3a542af096d118e8764a85b5087e82a7d3
kgyang <kaigeyang@sina.com>
2016-09-27 00:11:07 +0800

    Makefile

--- DHT/DHT.c
+++ DHT/DHT.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <bcm2835.h>
 
-#define DHT_GPIO.output() bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP) 
-#define DHT_GPIO.input() bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT)
-#define DHT_GPIO.set() bcm2835_gpio_set(pin)
-#define DHT_GPIO.clr() bcm2835_gpio_clr(pin)
-#define DHT_GPIO.read() bcm2835_gpio_lev(pin)
+#define DHT_GPIO_OUTPUT bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP) 
+#define DHT_GPIO_INPUT bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT)
+#define DHT_GPIO_SET bcm2835_gpio_set(pin)
+#define DHT_GPIO_CLR bcm2835_gpio_clr(pin)
+#define DHT_GPIO_READ bcm2835_gpio_lev(pin)
 #define DHT_wait_ms(ms) bcm2835_delay(ms)
 #define DHT_wait_us(us) bcm2835_delayMicroseconds(us)
 #define DHT_GPIO_HIGH HIGH
 #define DHT_GPIO_LOW LOW
 
-enum DHT_Type{
+typedef enum {
     DHT11   = 11,
     DHT21   = 21,
     DHT22   = 22,
     AM2302  = 22,
-};
+} DHT_Type ;
 
-enum DHT_Error {
+typedef enum {
     DHT_ERROR_NONE = 0,
     DHT_ERROR_BUS_BUSY = 1,
     DHT_ERROR_NOT_PRESENT =2,
     DHT_ERROR_TIMEOUT = 3,
     DHT_ERROR_CHECKSUM = 4,
-};
+} DHT_Error;
 
 
 int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
@@ -35,13 +35,13 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
     //         response signal to MCU.
     // If always signal high-voltage-level, it means DHT22 is not 
     // working properly, plesee check the electrical connection status.
-    DHT_GPIO.output();
+    DHT_GPIO_OUTPUT;
     DHT_wait_ms(18); //18 milliseconds
-    DHT_GPIO.clr(); //MCU send out start signal to DHT22
+    DHT_GPIO_CLR; //MCU send out start signal to DHT22
     DHT_wait_ms(18); //18 ms
-    DHT_GPIO.set(); //MCU pull up
+    DHT_GPIO_SET; //MCU pull up
     DHT_wait_us(40);        //40us
-    DHT_GPIO.input();
+    DHT_GPIO_INPUT;
 
     // Find the start of the ACK Pulse
     int count = 0;
@@ -50,7 +50,7 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
             return DHT_ERROR_NOT_PRESENT;;
         }
         DHT_wait_us(2);
-    }while(DHT_GPIO.read() == DTH_GPIO_HIGH);     // Exit on DHT22 pull low within 80us 
+    }while(DHT_GPIO_READ == DHT_GPIO_HIGH);     // Exit on DHT22 pull low within 80us 
     
     // Find the last of the ACK Pulse
     count = 0;
@@ -60,7 +60,7 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
             return DHT_ERROR_NOT_PRESENT;;
         }
         DHT_wait_us(2);
-    }while(DHT_GPIO.read() == DHT_GPIO_LOW);     // Exit on DHT22 pull High within 80us 
+    }while(DHT_GPIO_READ == DHT_GPIO_LOW);     // Exit on DHT22 pull High within 80us 
     
     // wait DHT pull low to sent zhe first bit.
     count = 0;
@@ -69,7 +69,7 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
             return DHT_ERROR_NOT_PRESENT;;
         }
         DHT_wait_us(2);
-    }while(DHT_GPIO.read() == DTH_GPIO_HIGH);     // Exit on DHT22 pull low  within 80us 
+    }while(DHT_GPIO_READ == DHT_GPIO_HIGH);     // Exit on DHT22 pull low  within 80us 
     
      
     // Reading the 40 bit data stream
@@ -79,6 +79,7 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
     //         1         -> high volage within 70us
     int idx = 0;
     int cnt = 0;
+    int i = 0;
     for (i = 0; i < 40; i++) {
         
         // Getting start bit signal 
@@ -89,7 +90,7 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
                 return DHT_ERROR_TIMEOUT;;
             }
             DHT_wait_us(2);
-        }while(DHT_GPIO.read() == DHT_GPIO_LOW);        // Exit on high volage within 50us
+        }while(DHT_GPIO_READ == DHT_GPIO_LOW);        // Exit on high volage within 50us
         
         // Measure the width of the data pulse
         count = 0;
@@ -99,7 +100,7 @@ int DHT_read(DHT_Type type, uint8_t pin, float* humidity, float* temperature)
                 return DHT_ERROR_TIMEOUT;;
             }
             DHT_wait_us(2);
-        }while(DHT_GPIO.read() == DTH_GPIO_HIGH);        // Exit on high volage within 50us
+        }while(DHT_GPIO_READ == DHT_GPIO_HIGH);        // Exit on high volage within 50us
         
         if(count > 20){            // connt > 20 is a 1(20*2 = 40us)
             data[idx] |= (1 << cnt);
