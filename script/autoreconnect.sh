e37435ae60432aac29fa404bc3428b588309d3c8
kgyang <kaigeyang@sina.com>
2016-10-17 22:45:26 +0800

    use cron to run autoreconnect

--- script/autoreconnect.sh
+++ script/autoreconnect.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-while true
-do
-	ifconfig wlan0 | grep inet >> /dev/null || sudo ifup --force wlan0
-	sleep 300
-done
+ifconfig wlan0 | grep inet >> /dev/null || sudo ifup --force wlan0
