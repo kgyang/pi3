0933c9cd556695c37888ccf17f9a245efb9759f4
kgyang <kaigeyang@sina.com>
2016-10-21 06:51:00 +0800

    correct grep error

--- script/autoreconnect.sh
+++ script/autoreconnect.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-while true
+log()
+{
+	log_file=/tmp/autoreconnect.log
+	echo "$(date): $1" >> $log_file
+}
+
+consecutive_failure=0
+while ((consecutive_failure < 100))
 do
-	ifconfig wlan0 | grep inet >> /dev/null || {
-		echo "$(date): wlan0 down, redo ifup" >> /tmp/autoreconnect.log
+	if ifconfig wlan0 | grep 'inet addr' >> /dev/null
+	then
+		consecutive_failure=0
+	else
+		let consecutive_failure++
+		log "wlan0 down, redo ifup"
 		sudo ifup --force wlan0
-	}
+	fi
 	sleep 300
 done
+log "failure"
 
