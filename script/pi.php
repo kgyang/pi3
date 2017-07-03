6eb6380a9e44d14a84d68150dae5e714b70f7f75
kgyang <kaigeyang@sina.com>
2017-07-03 23:45:03 +0800

    allow apache server to read ch2o sensor via uart

--- script/pi.php
+++ script/pi.php
@@ -1,27 +1,35 @@
-<h1>Welcome to YK&FLL's Home</h1>
+<?php
+echo "<h1> Welcome to YK&FLL's Home</h1>\n";
 
-<h2><?php echo date('Y-m-d H:i:s '); ?></h2>
-<p><?php system('/home/pi/pi3/script/temperature.sh'); ?></p>
+echo "<h2>";
+echo date('Y-m-d H:i:s ');
+echo "</h2>\n";
 
-<h2>History</h2>
-<?php 
-  $filename = '/home/pi/env_sample_data.txt';
-  $f = fopen($filename, 'r');
-  $ln = 0;
-  if ($f) {
+echo "<p>";
+system('/home/pi/pi3/script/temperature.sh');
+system('/home/pi/pi3/script/ch2o.py');
+echo "</p>\n";
+
+echo "<h2>History</h2>\n";
+
+$filename = '/home/pi/env_sample_data.txt';
+$f = fopen($filename, 'r');
+$ln = 0;
+if ($f) {
     while( fgets($f) != false) {
-      $ln++;
+        $ln++;
     }
     fseek($f, 0);
     $i = 0;
     while( ($line = fgets($f)) != false) {
-      $i++;
-      if (($ln - $i) <= 96) {
-        echo "<p>";
-        echo $line;
-        echo "</p>";
-      }
+        $i++;
+        if (($ln - $i) <= 48) {
+          echo "<p>";
+          echo $line;
+          echo "</p>\n";
+        }
     }
     fclose($f);
-  }
- ?>
+}
+
+?>
