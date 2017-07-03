4ec1e0297b7eb33b68aa18bf3060af044777b867
kgyang <kaigeyang@sina.com>
2017-07-03 23:44:12 +0800

    print error message to std error

--- script/ch2o.py
+++ script/ch2o.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python
 
+import sys 
 import time 
 import serial
 
 def read_ch2o():
     port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0)
-    raw = port.read(9)
     retries = 0;
-    while len(raw) != 9 and retries < 10:
+    while retries < 5:
+        raw = port.read(9)
+        if len(raw) == 9: break
         retries += 1
         time.sleep(1)
-        raw = port.read(9)
-    if (retries >= 10): return None
-    if (retries > 0): print 'retry times: ' + str(retries)
+    if (retries >= 5): return -1
+    #if (retries > 0): print 'retry times: ' + str(retries)
     data = [ord(x) for x in raw]
     if data[0] != 0xFF:
-        print 'invalid header'
-        return None
+        sys.stderr.write('invalid header\n')
+        return -1
     if data[1] != 0x17:
-        print 'invalid gas type format'
-        return None
+        sys.stderr.write('invalid gas type format\n')
+        return -1
     if data[2] != 0x04:
-        print 'invalid gas consentration unit'
-        return None
+        sys.stderr.write('invalid gas consentration unit\n')
+        return -1
     if data[3] != 0x04:
-        print 'invalid decimal digits'
-        return None
+        sys.stderr.write('invalid decimal digits\n')
+        return -1
     range_hi = data[6]
     range_lo = data[7]
     measurement_range = range_hi*256 + range_lo
     if measurement_range != 1000:
-        print 'invalid measurement range ' + str(measurement_range)
-        return None
+        sys.stderr.write('invalid measurement range ' + str(measurement_range) + '\n')
+        return -1
 
     chksum = data[8]
     calsum = 0
@@ -41,8 +42,8 @@ def read_ch2o():
     calsum = (~calsum) + 1
     if (calsum < 0): calsum += 256
     if calsum != chksum:
-        print 'check sum mismatch'
-        return None
+        sys.stderr.write('check sum mismatch\n')
+        return -1
 
     #valid data, now calculate gas consentration
     hi = data[4]
@@ -51,4 +52,6 @@ def read_ch2o():
     return val
 
 if __name__ == '__main__':
-    print str(read_ch2o())
+    gas = read_ch2o()
+    if gas >= 0:
+        print 'ch2o ' + str(gas)
